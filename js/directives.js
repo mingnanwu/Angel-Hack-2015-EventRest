03546168388af2f30fbc4e18cd8a57ca5d0a43f3
mingnanwu <mingnanwu@gmail.com>
2015-06-28 19:03:22 -0700

    Init, finished the hackathon

---
+++ js/directives.js
@@ -0,0 +1,32 @@
+"use strict";
+
+myApp.directive('inputtext', function ($timeout) {
+    return {
+        restrict:'E',
+        replace:true,
+        template:'<input type="text"/>',
+        scope: { 
+        	//if there were attributes it would be shown here
+        },
+        link:function (scope, element, attrs, ctrl) {
+        	// DOM manipulation may happen here.      
+        }
+    }
+});
+
+myApp.directive('version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+});
+
+
+myApp.directive("landing", function() {
+	return {
+		restrict: 'AE',
+      	replace: 'true',
+		templateUrl: 'partials/selectConfigTemplate.html'
+	};
+});
+
+// you may add as much directives as you want below
